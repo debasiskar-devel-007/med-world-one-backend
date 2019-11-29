cfa4e5d64d5a031722a6c0e5f69271b77df0e7d4
siladitya21 <siladitya.influxiq@gmail.com>
2019-11-30 01:18:01 +0530

    Friday 2am

--- src/app/Components/inventory/purchasecomparison/listing-purchase-comparison/listing-purchase-comparison.component.ts
+++ src/app/Components/inventory/purchasecomparison/listing-purchase-comparison/listing-purchase-comparison.component.ts
@@ -30,30 +30,51 @@ export class ListingPurchaseComparisonComponent implements OnInit {
   myControl = new FormControl();
   options: string[] = [];
   filteredOptions: Observable<string[]>;
-  sales_rep_array:any=[];
-  userData:any;
-  role:any;
-  headerFlag:string;
-  viewTable:string;
+  sales_rep_array: any = [];
+  userData: any;
+  role: any;
+  headerFlag: string;
+  viewTable: string;
+  id: any;
 
 
   /** View child **/
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
 
 
   constructor(private http: HttpServiceService, private cookieService: CookieService,
     private router: Router, public activatedRoute: ActivatedRoute, public dialog: MatDialog) {
     this.user_cookie = cookieService.get('jwtToken');
+    let allData: any = {};
+    allData = cookieService.getAll()
+    this.userData = JSON.parse(allData.user_details);
+    this.id = this.userData._id;
+
   }
 
   ngOnInit() {
-    this.activatedRoute.data.subscribe(resolveData => {
-      this.purchaseFormData = resolveData.data.res;
-      // console.log("----------",this.purchaseFormData);
-    });
-    this.datasource = new MatTableDataSource(this.purchaseFormData);
-    this.datasource.paginator = this.paginator;
-    console.log("DATALIST", this.datasource);
+
+    /** getting the header flag **/
+    this.headerFlag = this.activatedRoute.snapshot.url[0].path;
+
+    /** getting data for listing **/
+    if (this.headerFlag == 'admin') {
+      this.activatedRoute.data.subscribe(resolveData => {
+        this.purchaseFormData = resolveData.data.res;
+      });
+
+      setTimeout(() => {
+        this.datasource = new MatTableDataSource(this.purchaseFormData);
+        this.datasource.paginator = this.paginator;
+      }, 500);
+    }
+    else {
+      this.getHospitalForSalesRep();
+    }
+
+
+
+
 
     /** getting the salesrep names **/
     this.getSalesRepNames();
@@ -67,35 +88,30 @@ export class ListingPurchaseComparisonComponent implements OnInit {
         map(value => this._filter(value))
       );
 
-  
-      this.headerFlag = this.activatedRoute.snapshot.url[0].path;
-      console.log("->",this.headerFlag);
-     
-      if(this.headerFlag=='admin')
-        this.viewTable ="purchasecomparison_view_admin";
-        else
-        this.viewTable="purchasecomparison_view_rep"
+    if (this.headerFlag == 'admin')
+      this.viewTable = "purchasecomparison_view_admin";
+    else
+      this.viewTable = "purchasecomparison_view_rep"
 
   }
 
   /** filtered options for autocomplete**/
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-
     return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 
   /** quote view **/
   viewQuote(index: any) {
-    console.log("index", index);
     let data: any = {
-      'source': '"purchasecomparison_view_data',
+      'source': 'purchasecomparison_view_rep',
       'token': this.cookieService.get('jwtToken'),
       'condition': { '_id': index }
     }
     this.http.httpViaPost('datalist', data).subscribe(response => {
       let result = response.res;
-      this.openDialog(result[0].items);
+      this.openDialog(result[0].items)
+
     });
 
   }
@@ -116,7 +132,6 @@ export class ListingPurchaseComparisonComponent implements OnInit {
 
   /** searching data**/
   search_hospital(event: any) {
-    console.log("search value", event.target.value)
     let data: any = {
       'source': 'purchasecomparison_view_admin',
       'condition': {
@@ -134,8 +149,6 @@ export class ListingPurchaseComparisonComponent implements OnInit {
 
   /** search draft **/
   search_draft(event: any) {
-    console.log(event.value);
-
     let data: any = {
       'source': 'purchasecomparison_view_admin',
       'condition': {
@@ -145,7 +158,6 @@ export class ListingPurchaseComparisonComponent implements OnInit {
     }
     this.http.httpViaPost('datalist', data).subscribe(response => {
       let result = response.res;
-      console.log(result);
       this.datasource = new MatTableDataSource(result);
       this.datasource.paginator = this.paginator;
     });
@@ -164,9 +176,6 @@ export class ListingPurchaseComparisonComponent implements OnInit {
       for (let i = 0; i < result.length; i++) {
         this.options[i] = result[i].firstname + " " + result[i].lastname;
       }
-
-
-      console.log(this.options);
     });
   }
 
@@ -186,6 +195,26 @@ export class ListingPurchaseComparisonComponent implements OnInit {
     //   this.datasource.paginator = this.paginator;
     // });
 
+  }
+
+  /** salesrep hospital data **/
+  getHospitalForSalesRep() {
+    let data: any = {
+      source: 'purchasecomparison_view_admin',
+      condition: {
+        // 'type':'hospital',
+        'salesrep_id_object': this.id
+      },
+      token: this.cookieService.get('jwtToken')
+    }
+
+    this.http.httpViaPost('datalist', data).subscribe(response => {
+      this.purchaseFormData = response.res;
+      setTimeout(() => {
+        this.datasource = new MatTableDataSource(this.purchaseFormData);
+        this.datasource.paginator = this.paginator;
+      }, 500);
+    });
   }
 
 }
