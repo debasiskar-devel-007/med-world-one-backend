b11f622769aa8d5d5eb135f0414339ad716b4d45
Siladitya <siladitya.influxiq@gmail.com>
2019-12-02 19:18:26 +0530

    MOnday evening

--- src/app/Components/inventory/purchasecomparison/listing-purchase-comparison/listing-purchase-comparison.component.ts
+++ src/app/Components/inventory/purchasecomparison/listing-purchase-comparison/listing-purchase-comparison.component.ts
@@ -42,6 +42,7 @@ export class ListingPurchaseComparisonComponent implements OnInit {
   id: any;
   selected: { startDate: Moment, endDate: Moment };
   salesrep_id: any;
+  salesrep_name:string;
 
 
 
@@ -57,7 +58,7 @@ export class ListingPurchaseComparisonComponent implements OnInit {
     allData = cookieService.getAll()
     this.userData = JSON.parse(allData.user_details);
     this.id = this.userData._id;
-
+    this.salesrep_name = this.userData.firstname +' '+ this.userData.lastname;
   }
 
   ngOnInit() {
@@ -124,8 +125,6 @@ export class ListingPurchaseComparisonComponent implements OnInit {
 
   /** send email modal**/
   sendMailModal(index:any){
-    console.log('clicked');
-    console.log("vhck",index);
     this.openMailDialog("A");
 
   }
@@ -250,7 +249,7 @@ export class ListingPurchaseComparisonComponent implements OnInit {
     let data: any = {
       'source': 'purchasecomparison_view_admin',
       'condition': {
-        'date': { $lte: parseInt(startDate), $gte: parseInt(endDate) },
+        // 'date_added': { $lte: parseInt(endDate), $gte: parseInt(startDate) },
         'salesrep_id_object': this.salesrep_id
       },
       'token': this.cookieService.get('jwtToken')
