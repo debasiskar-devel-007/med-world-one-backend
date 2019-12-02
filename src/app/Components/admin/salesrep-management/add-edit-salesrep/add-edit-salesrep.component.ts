63dabbfe05d9b3853811fdcbe5e2599fb2832cfc
souravghatak-007 <sourav.influxiq@gmail.com>
2019-12-02 12:35:35 +0530

    user managemnet edit issues solve

--- src/app/Components/admin/salesrep-management/add-edit-salesrep/add-edit-salesrep.component.ts
+++ src/app/Components/admin/salesrep-management/add-edit-salesrep/add-edit-salesrep.component.ts
@@ -244,16 +244,20 @@ openDialog(x: any): void {
         "token": this.cookieService.get('jwtToken')
 
       };
-     
+      /**delete password when id not null */
+      if(postData.data.id){
+        //console.log("with ID");
+        delete postData.data.password;
+      }else{
+        //console.log("withOut ID");
+      }
+        //console.log(postData);
       this.http.httpViaPost('addorupdatedata', postData).subscribe((response: any) => {
-
         if (response.status == "success") {
           this.openDialog(this.successMessage);
           setTimeout(() => {
             this.dialogRef.close();
           }, 2000);
-
-
           this.router.navigateByUrl(this.linkTo);
         } else {
           alert("Some error occurred. Please try again.");
