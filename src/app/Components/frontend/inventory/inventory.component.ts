3495c3d4f695ebba9e026d597f42ead7aac3fb2e
subrata04 <subrata.influxiq@gmail.com>
2019-12-02 20:01:26 +0530

    subrata

--- src/app/Components/frontend/inventory/inventory.component.ts
+++ src/app/Components/frontend/inventory/inventory.component.ts
@@ -9,9 +9,11 @@ export class InventoryComponent implements OnInit {
 
 
   constructor() { }
-
+  categoryList = ['Ambulatory Equipment', 'Apparel',' Appliances (Durable Goods)', 'Body Pressure Relief and Positioning', 'Clinical Laboratory', 'Diagnostic Instruments and Supplies', 'Drainage and Suction', 'Furnishings', 'Gloves', 'Housekeeping',' I.V. Therapy', 'Implants', 'Incontinence', 'Indicators and Signage', 'Instruments', ' Needles and Syringes', 'Nurse+"s"+ Station and Office', 'Supplies', 'Nutritionals and Feeding', 'Supplies'
+  ,'Office Supplies', 'Orthopedic', 'Ostomy' ];
   ngOnInit() {
+  
 
   }
-
+ 
 }
