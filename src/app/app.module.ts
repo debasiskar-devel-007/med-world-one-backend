60c527bf1bf2a1eb4d89e4cea72a661aed298d43
Siladitya <siladitya.influxiq@gmail.com>
2019-12-13 19:44:45 +0530

    Friday evening

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -148,6 +148,8 @@ import { QuoteViewComponent } from './Components/backend/sales-rep/quote-view/qu
 
 //admin dashboard view purchase quote
 import { PurchaseQuotesListingComponent } from './Components/admin/purchase-quotes-listing/purchase-quotes-listing.component';
+import { PurchaseComparisonSearchListComponent } from './Components/inventory/purchase-comparison-search-list/purchase-comparison-search-list.component';
+import { AdminInventoryDetailsComponent } from './Components/inventory/admin-inventory-details/admin-inventory-details.component';
 
 
 
@@ -250,7 +252,7 @@ import { PurchaseQuotesListingComponent } from './Components/admin/purchase-quot
     Dialoglogin,
     Dialogloginn,
     quoteModal,sendMailModal, AddcontactinfoComponent, QuotesCartComponent, AdminDashboardHospitalViewdetailsComponent, AdminDetailsComponent, QuoteViewComponent,
-    quoteModal,sendMailModal, AddcontactinfoComponent, QuotesCartComponent, AdminDashboardHospitalViewdetailsComponent, AdminDetailsComponent, PurchaseQuotesListingComponent
+    quoteModal,sendMailModal, AddcontactinfoComponent, QuotesCartComponent, AdminDashboardHospitalViewdetailsComponent, AdminDetailsComponent, PurchaseQuotesListingComponent, PurchaseComparisonSearchListComponent, AdminInventoryDetailsComponent
 
 
   ],
