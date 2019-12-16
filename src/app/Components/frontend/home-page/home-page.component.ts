589b90de6a489607cf2479fa04113c2d928c4828
koushikbiswas <biswakoushik.influxiq@gmail.com>
2019-12-16 13:18:55 +0530

    koushik blog

--- src/app/Components/frontend/home-page/home-page.component.ts
+++ src/app/Components/frontend/home-page/home-page.component.ts
@@ -3,6 +3,7 @@ import { CookieService } from 'ngx-cookie-service';
 import { HttpServiceService } from '../../../services/http-service.service';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Router, ActivatedRoute } from '@angular/router';
+import { MetaService } from '@ngx-meta/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -11,7 +12,23 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class HomePageComponent implements OnInit {
   public api_url:any =this.httpServiceService.baseUrl;
   public popularInventoryDetails:any=[];
-  constructor(public router:Router,public httpClient:HttpClient, public cookieService: CookieService,public httpServiceService: HttpServiceService) {
+  constructor(public router:Router,public httpClient:HttpClient, public cookieService: CookieService,public httpServiceService: HttpServiceService, private readonly meta: MetaService) {
+
+    this.meta.setTitle('MD Stock International - Your Medical Partner');
+    this.meta.setTag('og:description', 'MD Stick International is the Medical Partner you want to have to source Premium Hospital Equipment and Supplies for your Hospital, Clinic or other medical facilities.');
+    this.meta.setTag('twitter:description', 'MD Stick International is the Medical Partner you want to have to source Premium Hospital Equipment and Supplies for your Hospital, Clinic or other medical facilities.');
+
+    this.meta.setTag('og:keyword', 'MD Stock International, Hospital Equipment, Hospital Machinery, Premium Hospital Supplies, Used Medical Equipment, Used Hospital Equipment, Medical Partner');
+    this.meta.setTag('twitter:keyword', 'MD Stock International, Hospital Equipment, Hospital Machinery, Premium Hospital Supplies, Used Medical Equipment, Used Hospital Equipment, Medical Partner');
+
+    this.meta.setTag('og:title', 'MD Stock International - Your Medical Partner');
+    this.meta.setTag('twitter:title', 'MD Stock International - Your Medical Partner');
+    this.meta.setTag('og:type', 'website');
+    this.meta.setTag('og:image', 'https://dev.mdstockinternational.com/assets/images/logo.png');
+    this.meta.setTag('twitter:image', 'https://dev.mdstockinternational.com/assets/images/logo.png');
+
+
+
     const link = this.api_url + 'temptoken';
     this.httpClient.post(link, {}).subscribe(res => {
     let result: any = res;
--- src/app/Components/frontend/manufacturar-direct/manufacturar-direct.component.ts
+++ src/app/Components/frontend/manufacturar-direct/manufacturar-direct.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MetaService } from '@ngx-meta/core';
 
 @Component({
   selector: 'app-manufacturar-direct',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManufacturarDirectComponent implements OnInit {
 
-  constructor() { }
+  constructor(private readonly meta: MetaService) { 
+    this.meta.setTitle('MD Stock International - Manufacturer Direct');
+      this.meta.setTag('og:description', 'Buy your Hospital, Laboratory and Medical Equipment and Supplies directly from the Manufacturer and get top-quality machinery and items without having to spend a fortune.');
+      this.meta.setTag('twitter:description', 'Buy your Hospital, Laboratory and Medical Equipment and Supplies directly from the Manufacturer and get top-quality machinery and items without having to spend a fortune.');
+
+      this.meta.setTag('og:keyword', 'Manufacturer Direct Medical Equipment, Buy Medical Equipment Directly From Manufacturer, Buy Medical Supplies Directly From Manufacturer');
+      this.meta.setTag('twitter:keyword', 'Manufacturer Direct Medical Equipment, Buy Medical Equipment Directly From Manufacturer, Buy Medical Supplies Directly From Manufacturer');
+
+      this.meta.setTag('og:title', 'MD Stock International - Manufacturer Direct');
+      this.meta.setTag('twitter:title', 'MD Stock International - Manufacturer Direct');
+      this.meta.setTag('og:type', 'website');
+      this.meta.setTag('og:image', 'https://dev.mdstockinternational.com/assets/images/logo.png');
+      this.meta.setTag('twitter:image', 'https://dev.mdstockinternational.com/assets/images/logo.png');
+  }
 
   ngOnInit() {
   }
