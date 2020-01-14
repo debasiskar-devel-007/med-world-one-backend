8ae0db4b1715ea03ffee8443915d72003f4e6bdc
souravghatak-007 <sourav.influxiq@gmail.com>
2020-01-14 18:03:57 +0530

    edit department

--- src/app/Components/admin/department/department.component.ts
+++ src/app/Components/admin/department/department.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CookieService } from 'ngx-cookie-service';
 import { HttpServiceService } from 'src/app/services/http-service.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ThrowStmt } from '@angular/compiler';
+
 @Component({
   selector: 'app-department',
   templateUrl: './department.component.html',
@@ -10,18 +13,36 @@ import { HttpServiceService } from 'src/app/services/http-service.service';
 export class DepartmentComponent implements OnInit {
 public departmentForm:FormGroup;
 public deplist:any=[];
+public defaultvalue:any
   constructor(public formBuilder: FormBuilder, public http: HttpServiceService,
-    public cookieService: CookieService) {
-
+    public cookieService: CookieService,public activatedRoute:ActivatedRoute) {
+      //console.log(this.activatedRoute.snapshot.params.id);
+      if(this.activatedRoute.snapshot.params.id){
+        let post={
+          "source": 'department',
+          "condition":{
+          "_id_object":this.activatedRoute.snapshot.params.id
+          }
+        }
+        this.http.httpViaPost('datalist',post).subscribe((res:any)=>{
+          
+          setTimeout(() => {
+            this.defaultvalue=res.res[0];
+            this.patchval(this.defaultvalue);
+          }, 1000);
+        })
+       
+      }
       let post={
         "source": 'department',
       }
       this.http.httpViaPost('datalist',post).subscribe((res:any)=>{
         this.deplist=res.res;
-        console.warn(res);
+        //console.warn(res);
       })
 
       this.departmentForm = this.formBuilder.group({
+        id:[null],
        department_name:['',Validators.required],
        department_description:['',Validators.required],
        department_priority:['',Validators.required],
@@ -31,23 +52,48 @@ public deplist:any=[];
      }
 
   ngOnInit() {
-  }
 
+  }
+  patchval(item:any){
+    this.departmentForm.patchValue({
+      id:item._id,
+      department_name:item.department_name,
+       department_description:item.department_description,
+       department_priority:item.department_priority,
+       department_parent:item.department_parent,
+       status:item.status
+    })
+  }
     /** blur function **/
     inputBlur(val: any) {
       this.departmentForm.controls[val].markAsUntouched();
     }
 /**onsubmit */
 onSubmit(){
+  
   for (let x in this.departmentForm.controls) {
     this.departmentForm.controls[x].markAsTouched();
   }
-  let postData: any = {
-    "source": 'department',
-    "data":this.departmentForm.value,
-    "sourceobj":["department_parent"]
-      };
+
+  if(this.departmentForm.value.id==null){
+    delete this.departmentForm.value.id
+    var postData: any = {
+      "source": 'department',
+      "data":this.departmentForm.value,
+      "sourceobj":["department_parent"]
+        };
+  }else{
+    var postData: any = {
+
+      "source": 'department',
+       "data":this.departmentForm.value,
+   
+      "sourceobj":["department_parent"]
+        };
+  }
+
       //console.log(postData);
+
       this.http.httpViaPost('addorupdatedata', postData).subscribe((response: any) => {
         //console.log(response);
         if (response.status == "success") {
